7f6a48af9b73742e917f080caf7d43c35a489c9f
MTbliz <turlinskimichal@gmail.com>
2020-03-09 02:11:55 +0100

    change in validation

--- src/main/java/com/example/booking_app/model/User.java
+++ src/main/java/com/example/booking_app/model/User.java
@@ -3,9 +3,9 @@ package com.example.booking_app.model;
 import javax.validation.constraints.Pattern;
 
 public class User {
-    @Pattern(regexp="^[A-ZŻŹĆĄŚĘŁÓŃ]([a-zżźćńółęąś]){2,}([a-zżźćńółęąś])$",message="FirstName length must be at least 3 characters and starts from Capital letter.")
+    @Pattern(regexp="^[A-ZŻŹĆĄŚĘŁÓŃ]([a-zżźćńółęąś]){1,}([a-zżźćńółęąś])$",message="FirstName length must be at least 3 characters and starts from Capital letter.")
     private String firstName;
-    @Pattern(regexp="^[A-ZŻŹĆĄŚĘŁÓŃ]([a-zżźćńółęąś]){2,}([a-zżźćńółęąś])$|^[A-ZŻŹĆĄŚĘŁÓŃ]([a-zżźćńółęąś]){0,}[-][A-ZŻŹĆĄŚĘŁÓŃ]([a-zżźćńółęąś])*",message="LastName length must be at least 3 characters, contains only letters or letters with single dash (two prats should start with capital letter)")
+    @Pattern(regexp="^[A-ZŻŹĆĄŚĘŁÓŃ]([a-zżźćńółęąś]){1,}([a-zżźćńółęąś])$|^[A-ZŻŹĆĄŚĘŁÓŃ]([a-zżźćńółęąś]){0,}[-][A-ZŻŹĆĄŚĘŁÓŃ]([a-zżźćńółęąś])*",message="LastName length must be at least 3 characters, contains only letters or letters with single dash (two prats should start with capital letter)")
     private String lastName;
 
     public String getFirstName() {
